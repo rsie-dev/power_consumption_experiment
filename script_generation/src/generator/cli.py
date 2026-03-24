7d2ccb5733ace076f8428124cec7eaec216e0b29
Ralf Sieger <sieger@fzi.de>
2026-03-24 09:58:25 +0100

    initial generator

---
+++ script_generation/src/generator/cli.py
@@ -0,0 +1,55 @@
+import logging.config
+import argparse
+from pathlib import Path
+
+from ruamel.yaml import YAML
+
+
+class Generator:
+    def __init__(self):
+        self._logger = logging.getLogger(self.__class__.__name__)
+
+    def _get_app_folder(self):
+        script = Path(__file__).resolve()
+        folder = script.parent
+        return folder
+
+    def _start_logging(self, args):
+        log_file_name = args.logFile
+        num_log_level = 50 - min(4, 2 + args.verbose) * 10
+        log_level = logging.getLevelName(num_log_level)
+
+        yaml_config = self._get_logging_config()
+        yaml_config['handlers']['console']['level'] = log_level
+        if log_file_name:
+            yaml_config['handlers']['file']['filename'] = log_file_name
+        logging.config.dictConfig(yaml_config)
+
+    def _get_logging_config(self):
+        folder = self._get_app_folder()
+        config = folder / 'logging.yaml'
+        with open(config, "rt", encoding="UTF_8") as f:
+            yaml = YAML(typ="safe")
+            return yaml.load(f)
+
+    def main(self):
+        parser = argparse.ArgumentParser()
+        default = ' (default: %(default)s)'
+        parser.add_argument('-v', '--verbose', action='count', default=1, help="set the verbosity level" + default)
+        parser.add_argument('-l', '--logFile', help="logfile name")
+        args = parser.parse_args()
+
+        self._start_logging(args)
+        try:
+            # ToDo:
+            return 0
+        except KeyboardInterrupt:
+            self._logger.warning("User cancel")
+        except Exception as e:  # pylint: disable=broad-exception-caught
+            self._logger.exception("Error: %s", e)
+        return 1
+
+
+def app():
+    generator = Generator()
+    return generator.main()
