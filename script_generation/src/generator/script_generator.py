7fddddb4370447c8b367f94da07aec2087cb825c
Ralf Sieger <sieger@fzi.de>
2026-03-25 10:47:21 +0100

    added support for multiple tools per script

--- script_generation/src/generator/script_generator.py
+++ script_generation/src/generator/script_generator.py
@@ -16,51 +16,62 @@ class ScriptGenerator:
             loader=PackageLoader("generator")
         )
 
-        tool: Tool = Tool.bzip2
-        print("tool: %s" % tool.name)
-
         template = env.get_template('experiment.jinja')
+
         tool_config = ToolConfig(mode=OperationMode.COMPRESS, strength=CompressionStrength.DEFAULT,
                                  threading=Threading.SINGLE)
+
+        tools = []
+        tools.append(self._build_tool_entry(Tool.lzop, tool_config))
+        tools.append(self._build_tool_entry(Tool.gzip, tool_config))
+
         data = {
             "runs": 10,
             "host": "raspi5",
             "ip": "192.168.1.102",
             "multimeter": "07D1A5642160",
             #"head_delay": 2,
-            "tool": tool.name,
-            "tool_tags": self._get_tool_tags(tool, tool_config),
-            "tool_args": self._get_tool_config(tool, tool_config),
+            "tools": tools,
             "input_file": "test.data",
         }
         output = template.render(data)
         print(output)
 
-    def _get_tool_config(self, tool: Tool, config: ToolConfig):
-        tool_args = []
-        if config.mode == OperationMode.COMPRESS:
-            tool_args.append(tool.compress)
-        else:
-            tool_args.append(tool.decompress)
+    def _build_tool_entry(self, tool: Tool, tool_config: ToolConfig):
+        entry = {
+            "tool": tool,
+            "tool_tags": _get_tool_tags(tool_config),
+            "tool_args": _get_tool_config(tool, tool_config),
+        }
+        return entry
 
-        tool_args.extend([tool.keep, tool.to_stdout])
 
-        if config.strength == CompressionStrength.MIN:
-            tool_args.append(tool.min)
-        elif config.strength == CompressionStrength.MAX:
-            tool_args.append(tool.max)
+def _get_tool_config(tool: Tool, config: ToolConfig):
+    tool_args = []
+    if config.mode == OperationMode.COMPRESS:
+        tool_args.append(tool.compress)
+    else:
+        tool_args.append(tool.decompress)
 
-        if config.threading == Threading.SINGLE:
-            tool_args.append(tool.single_thread)
-        else:
-            tool_args.append(tool.multi_thread)
+    tool_args.extend([tool.keep, tool.to_stdout])
 
-        return " ".join(tool_args)
+    if config.strength == CompressionStrength.MIN:
+        tool_args.append(tool.min)
+    elif config.strength == CompressionStrength.MAX:
+        tool_args.append(tool.max)
 
-    def _get_tool_tags(self, tool: Tool, config: ToolConfig):
-        tags = []
-        tags.append(config.mode.name.lower())
-        if config.mode == OperationMode.COMPRESS:
-            tags.append(config.strength.name.lower())
-        tags.append(config.threading.name.lower())
-        return tags
+    if config.threading == Threading.SINGLE:
+        tool_args.append(tool.single_thread)
+    else:
+        tool_args.append(tool.multi_thread)
+
+    return " ".join(tool_args)
+
+
+def _get_tool_tags(config: ToolConfig):
+    tags = []
+    tags.append(config.mode.name.lower())
+    if config.mode == OperationMode.COMPRESS:
+        tags.append(config.strength.name.lower())
+    tags.append(config.threading.name.lower())
+    return tags
