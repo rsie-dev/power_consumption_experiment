ab41e8e98b01882cc9285c362f9ae49958a0e5b8
Ralf Sieger <sieger@fzi.de>
2026-04-03 13:48:28 +0200

    added baseline generation

---
+++ script_generation/src/generator/baseline_script_generator.py
@@ -0,0 +1,24 @@
+import logging
+from pathlib import Path
+
+from generator.script_generator import ScriptGenerator
+from generator.tools import Tool
+from generator.data_set import DataSet
+
+
+class BaselineScriptGenerator(ScriptGenerator):
+    def __init__(self, script_folder: Path):
+        super().__init__(script_folder)
+        self._logger = logging.getLogger(self.__class__.__name__)
+
+    def _get_template_name(self) -> str:
+        return "baseline.jinja"
+
+    def _write_scripts(self, tools: list[Tool], data_sets: list[DataSet], template, args) -> None:
+        data = {
+            "args": args,
+            "sleep_time": 15,
+        }
+
+        host_script = self._script_folder / f"{args.host}_baseline.py"
+        self._generate_script(host_script, template, data)
--- script_generation/src/generator/generator_type.py
+++ script_generation/src/generator/generator_type.py
@@ -8,6 +8,7 @@ from generator.mode_script_generator import ModeScriptGenerator
 from generator.tool_script_generator import ToolScriptGenerator
 from generator.dataset_script_generator import DataSetScriptGenerator
 from generator.data_gen_script_generator import DataGenScriptGenerator
+from generator.baseline_script_generator import BaselineScriptGenerator
 
 
 class GeneratorType(Enum):
@@ -16,6 +17,7 @@ class GeneratorType(Enum):
     TOOL = ToolScriptGenerator
     DATASET = DataSetScriptGenerator
     DATAGEN = DataGenScriptGenerator
+    BASELINE = BaselineScriptGenerator
 
     def create(self, script_folder: Path) -> ScriptGenerator:
         generator_class: Type[ScriptGenerator] = self.value
