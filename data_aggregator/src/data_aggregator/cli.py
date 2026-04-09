16aa1ed8a5d6225e8ffb0c0c703a65b72a188f1c
Ralf Sieger <sieger@fzi.de>
2026-04-09 16:35:09 +0200

    fixed argument name

--- data_aggregator/src/data_aggregator/cli.py
+++ data_aggregator/src/data_aggregator/cli.py
@@ -79,7 +79,7 @@ class Processor:
         resources_folder = args.resources
         resources_folder.mkdir(parents=True, exist_ok=True)
         calculator = AverageCalculator(resources_folder)
-        calculator.calculate(args.single_run_data_frame)
+        calculator.calculate(args.run_data_frame)
 
     def main(self):
         parser = argparse.ArgumentParser()
