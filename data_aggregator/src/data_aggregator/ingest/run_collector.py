c88bc4db4b98c7a19d439fca1aef21185adc9b85
Ralf Sieger <sieger@fzi.de>
2026-04-20 10:38:00 +0200

    adapted to changed raw data format

--- data_aggregator/src/data_aggregator/ingest/run_collector.py
+++ data_aggregator/src/data_aggregator/ingest/run_collector.py
@@ -1,15 +1,14 @@
 import logging
 from pathlib import Path
-import csv
 import datetime
 from typing import Generator
-
-import pandas as pd
 
 from data_aggregator.common import OperationMode
 from data_aggregator.common import RunInfo
 from data_aggregator.common import Timings, Measurement
 from data_aggregator.common import MeasurementInfo
+from data_aggregator.util import FrameIO
+from data_aggregator import ureg
 
 
 class RunCollector:
@@ -28,52 +27,35 @@ class RunCollector:
 
     def collect_run(self, mode: OperationMode, run_folder: Path) -> RunInfo:
         self._logger.debug("processing run folder: %s", run_folder)
+        frame_io = FrameIO()
         run = int(run_folder.stem[4:])
         count = None
         count_file = run_folder / 'count_stdout.csv'
         if mode == OperationMode.COMPRESS:
-            with open(count_file, encoding="utf-8") as csvfile:
-                reader = csv.DictReader(csvfile)
-                first = next(iter(reader))
-                count = int(first["count_B"])
+            df = frame_io.load(count_file)
+            count_quantity = df["count"].iloc[0]
+            count = int(count_quantity.to(ureg.byte).magnitude)
 
         end, start = self._read_markers(run_folder)
-
         timings = self._read_timings(run_folder)
 
-        readings = self._read_measurement(run_folder, run)
+        readings = frame_io.load(run_folder / 'multimeter.csv')
+        readings['run'] = run
         measurement = Measurement(start=start, end=end, count=count, timings=timings, readings=readings)
         return RunInfo(run=run, measurement=measurement)
 
     def _read_timings(self, run_folder):
-        with open(run_folder / 'timings.csv', encoding="utf-8") as csvfile:
-            reader = csv.DictReader(csvfile)
-            it = iter(reader)
-            start_entry = next(it)
-            real = datetime.timedelta(seconds=float(start_entry["real_S"]))
-            user = datetime.timedelta(seconds=float(start_entry["user_S"]))
-            sys = datetime.timedelta(seconds=float(start_entry["sys_S"]))
-            timings = Timings(real=real, user=user, sys=sys)
+        frame_io = FrameIO()
+        df = frame_io.load(run_folder / 'timings.csv')
+        real = datetime.timedelta(seconds=df["real"].iloc[0].to(ureg.second).magnitude)
+        user = datetime.timedelta(seconds=df["user"].iloc[0].to(ureg.second).magnitude)
+        sys = datetime.timedelta(seconds=df["sys"].iloc[0].to(ureg.second).magnitude)
+        timings = Timings(real=real, user=user, sys=sys)
         return timings
 
     def _read_markers(self, run_folder):
-        with open(run_folder / 'markers.csv', encoding="utf-8") as csvfile:
-            reader = csv.DictReader(csvfile)
-            it = iter(reader)
-            start_entry = next(it)
-            start = datetime.datetime.fromisoformat(start_entry["timestamp"])
-            end_entry = next(it)
-            end = datetime.datetime.fromisoformat(end_entry["timestamp"])
+        frame_io = FrameIO()
+        df = frame_io.load(run_folder / 'markers.csv')
+        start = df.loc[df["kind"] == "START", "timestamp"].iloc[0]
+        end = df.loc[df["kind"] == "END", "timestamp"].iloc[0]
         return end, start
-
-    def _read_measurement(self, run_folder: Path, run: int) -> pd.DataFrame:
-        #self._logger.debug("read: %s", (run_folder / 'multimeter.csv'))
-        df = pd.read_csv(run_folder / 'multimeter.csv', parse_dates=["timestamp"], date_format="ISO8601",
-                         skipinitialspace=True)
-        df["voltage_V"] = df["voltage_V"].astype("pint[volt]")
-        df["current_A"] = df["current_A"].astype("pint[ampere]")
-        df = df.rename(columns={'voltage_V': 'voltage', 'current_A': 'current'})
-        df = df.drop('temperature_C', axis=1)
-        df['run'] = run
-        df = df[['run', 'timestamp', "voltage", "current"]]
-        return df
