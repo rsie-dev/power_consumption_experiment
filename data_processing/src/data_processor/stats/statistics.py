d9d2f6e11ea5573a6fa54bfd09d38fddcd835179
Ralf Sieger <sieger@fzi.de>
2026-06-17 17:23:39 +0200

    added times and sizes

--- data_processing/src/data_processor/stats/statistics.py
+++ data_processing/src/data_processor/stats/statistics.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from statistics import mean, stdev
 
 import tabulate
+import humanize
 
 from data_processor.util import FrameIO
 
@@ -22,9 +23,17 @@ class Statistics:
         for group_name, group_df in df.groupby(group_cols):
             unit_power = str(group_df["power"].dtype.units)
             values_power = group_df["power"].pint.magnitude
+            unit_times = str(group_df["real"].dtype.units)
+            values_times = group_df["real"].pint.magnitude
+            unit_size = str(group_df["size"].dtype.units)
+            values_size = group_df["size"].pint.magnitude
 
             table_entries = []
             table_entries.append(self._get_stats("power", unit_power, values_power))
+            table_entries.append(self._get_stats("times", unit_times, values_times))
+            size_entries = self._get_stats("size", unit_size, values_size)
+            entries = list(size_entries[:2]) + [humanize.naturalsize(s) for s in size_entries[2:]]
+            table_entries.append(entries)
 
             headers = ["item", "unit", "min", "max", "mean", "std"]
             table_str = tabulate.tabulate(table_entries,
