34e4e9ed8f3592ff2d7de7d91c9a453a26a94d73
Ralf Sieger <sieger@fzi.de>
2026-06-19 14:06:26 +0200

    adapted to changed input file

--- data_processing/src/data_processor/stats/statistics.py
+++ data_processing/src/data_processor/stats/statistics.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from statistics import mean, stdev
 
 import tabulate
+from tabulate import SEPARATING_LINE
 import humanize
 
 from data_processor.util import FrameIO
@@ -20,19 +21,29 @@ class Statistics:
 
     def _list_statistics(self, df):
         group_cols = ["host", "tool", "dataset", "mode", "strength", "threading"]
-        for group_name, group_df in df.groupby(group_cols):
-            group = "_".join(group_name)
-            self._process_group(group, group_df)
-
-    def _process_group(self, group: str, df):
-        unit_power = str(df["power"].dtype.units)
-        values_power = df["power"].pint.magnitude
+        unit_energy = str(df["energy"].dtype.units)
         unit_times = str(df["real"].dtype.units)
-        values_times = df["real"].pint.magnitude
         unit_size = str(df["size"].dtype.units)
+        table_entries = []
+        for _, group_df in df.groupby(group_cols):
+            entries = self._process_group(group_df)
+            table_entries.extend(entries)
+            table_entries.append(SEPARATING_LINE)
+
+        headers = ["stat", "host", "tool", "dataset", "mode", "strength", "threading",
+                   "energy (%s)" % unit_energy, "times (%s)" % unit_times, "size (%s)" % unit_size]
+        table_str = tabulate.tabulate(table_entries,
+                                      headers=headers,
+                                      tablefmt="simple"
+                                      )
+        print(table_str)
+
+    def _process_group(self, df) -> list:
+        values_energy = df["energy"].pint.magnitude
+        values_times = df["real"].pint.magnitude
         values_size = df["size"].pint.magnitude
 
-        stats_power = self._post_process_stats(self._get_stats(values_power))
+        stats_energy = self._post_process_stats(self._get_stats(values_energy))
         stats_times = self._post_process_stats(self._get_stats(values_times))
         stats_sizes = {}
         for k, v in self._get_stats(values_size).items():
@@ -45,18 +56,21 @@ class Statistics:
                 stats_sizes[k] = humanize.naturalsize(v, binary=True)
 
         table_entries = []
-        table_entries.append(("min", stats_power["min"], stats_times["min"], stats_sizes["min"]))
-        table_entries.append(("max", stats_power["max"], stats_times["max"], stats_sizes["max"]))
-        table_entries.append(("mean", stats_power["mean"], stats_times["mean"], stats_sizes["mean"]))
-        table_entries.append(("std", stats_power["stdev"], stats_times["stdev"], stats_sizes["stdev"]))
-
-        headers = ["stat", "power (%s)" % unit_power, "times (%s)" % unit_times, "size (%s)" % unit_size]
-        table_str = tabulate.tabulate(table_entries,
-                                      headers=headers,
-                                      tablefmt="simple"
-                                      )
-        print(f"Group: {group}")
-        print(table_str)
+        host = df["host"].iloc[0]
+        tool = df["tool"].iloc[0]
+        dataset = df["dataset"].iloc[0]
+        mode = df["mode"].iloc[0]
+        strength = df["strength"].iloc[0]
+        threading = df["threading"].iloc[0]
+        table_entries.append(("min", host, tool, dataset, mode, strength, threading,
+                              stats_energy["min"], stats_times["min"], stats_sizes["min"]))
+        table_entries.append(("max", host, tool, dataset, mode, strength, threading,
+                              stats_energy["max"], stats_times["max"], stats_sizes["max"]))
+        table_entries.append(("mean", host, tool, dataset, mode, strength, threading,
+                              stats_energy["mean"], stats_times["mean"], stats_sizes["mean"]))
+        table_entries.append(("std", host, tool, dataset, mode, strength, threading,
+                              stats_energy["stdev"], stats_times["stdev"], stats_sizes["stdev"]))
+        return table_entries
 
     def _post_process_stats(self, stats: dict) -> dict:
         processed_stats = {}
