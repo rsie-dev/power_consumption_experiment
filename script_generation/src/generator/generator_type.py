07a9f0496b184c0b2f4513ea4ab5ffcd6f7b66d1
Ralf Sieger <sieger@fzi.de>
2026-07-07 13:27:43 +0200

    removed dataset script generator

--- script_generation/src/generator/generator_type.py
+++ script_generation/src/generator/generator_type.py
@@ -4,14 +4,12 @@ from pathlib import Path
 
 from generator.script_generator import ScriptGenerator
 from generator.host_script_generator import HostScriptGenerator
-from generator.dataset_script_generator import DataSetScriptGenerator
 from generator.data_gen_script_generator import DataGenScriptGenerator
 from generator.baseline_script_generator import BaselineScriptGenerator
 
 
 class GeneratorType(Enum):
     HOST = HostScriptGenerator
-    DATASET = DataSetScriptGenerator
     DATAGEN = DataGenScriptGenerator
     BASELINE = BaselineScriptGenerator
 
