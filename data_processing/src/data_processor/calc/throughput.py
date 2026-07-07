8c62cebec007aaf478f552d69cd0e6f67cbaa728
Ralf Sieger <sieger@fzi.de>
2026-07-07 08:46:55 +0200

    simplified the code

--- data_processing/src/data_processor/calc/throughput.py
+++ data_processing/src/data_processor/calc/throughput.py
@@ -11,12 +11,12 @@ from data_processor.data_set import dataset_from_str
 class Throughput:
     def __init__(self, resources: Path):
         self._logger = logging.getLogger(self.__class__.__name__)
+        self._frameio = FrameIO()
         self._resources = resources
 
     def process(self, used_energy_file: Path, create_tex: bool, no_tool: list, no_dataset: list):
-        frameio = FrameIO()
         self._logger.debug("loading %s", used_energy_file)
-        df = frameio.load(used_energy_file)
+        df = self._frameio.load(used_energy_file)
         df = df[~df["tool"].isin(no_tool)]
         df = df[~df["dataset"].isin(no_dataset)]
 
@@ -41,7 +41,7 @@ class Throughput:
 
         headers = cols[:-2]
         headers.append("average real (s)")
-        headers.append("average throughput (MiB/s")
+        headers.append("average throughput (MiB/s)")
         table_str = tabulate.tabulate(table_entries,
                                       headers=headers,
                                       tablefmt="simple"
@@ -50,8 +50,7 @@ class Throughput:
 
     def _create_csv(self, used_energy_file: Path, df: pd.DataFrame):
         tp_file = self._resources / ("tp_%s" % used_energy_file.stem.removeprefix("used_energy_") + ".csv")
-        frameio = FrameIO()
-        frameio.persist(df, tp_file)
+        self._frameio.persist(df, tp_file)
 
     def _calculate_throughput(self, df: pd.DataFrame) -> pd.DataFrame:
         group_cols = ["host", "tool", "dataset", "mode", "strength", "threading"]
