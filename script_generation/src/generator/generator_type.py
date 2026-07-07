344c0fc56f73cd3707cf28889cd97d97d31e0d63
Ralf Sieger <sieger@fzi.de>
2026-07-07 13:26:59 +0200

    removed mode script generator

--- script_generation/src/generator/generator_type.py
+++ script_generation/src/generator/generator_type.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 from generator.script_generator import ScriptGenerator
 from generator.host_script_generator import HostScriptGenerator
-from generator.mode_script_generator import ModeScriptGenerator
 from generator.dataset_script_generator import DataSetScriptGenerator
 from generator.data_gen_script_generator import DataGenScriptGenerator
 from generator.baseline_script_generator import BaselineScriptGenerator
@@ -12,7 +11,6 @@ from generator.baseline_script_generator import BaselineScriptGenerator
 
 class GeneratorType(Enum):
     HOST = HostScriptGenerator
-    MODE = ModeScriptGenerator
     DATASET = DataSetScriptGenerator
     DATAGEN = DataGenScriptGenerator
     BASELINE = BaselineScriptGenerator
