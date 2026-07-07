1f9d661522ec83699c4cc70eea886bb9d1831679
Ralf Sieger <sieger@fzi.de>
2026-07-07 13:26:16 +0200

    removed tool script generator

--- script_generation/src/generator/generator_type.py
+++ script_generation/src/generator/generator_type.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from generator.script_generator import ScriptGenerator
 from generator.host_script_generator import HostScriptGenerator
 from generator.mode_script_generator import ModeScriptGenerator
-from generator.tool_script_generator import ToolScriptGenerator
 from generator.dataset_script_generator import DataSetScriptGenerator
 from generator.data_gen_script_generator import DataGenScriptGenerator
 from generator.baseline_script_generator import BaselineScriptGenerator
@@ -14,7 +13,6 @@ from generator.baseline_script_generator import BaselineScriptGenerator
 class GeneratorType(Enum):
     HOST = HostScriptGenerator
     MODE = ModeScriptGenerator
-    TOOL = ToolScriptGenerator
     DATASET = DataSetScriptGenerator
     DATAGEN = DataGenScriptGenerator
     BASELINE = BaselineScriptGenerator
