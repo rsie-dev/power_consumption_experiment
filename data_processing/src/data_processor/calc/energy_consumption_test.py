cdbcb09f6c7641499662c1e6e9a4fc14cb69e2e0
Ralf Sieger <sieger@fzi.de>
2026-07-24 13:55:00 +0200

    added pint units

--- data_processing/src/data_processor/calc/energy_consumption_test.py
+++ data_processing/src/data_processor/calc/energy_consumption_test.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import pytest
 import pandas as pd
 
+
+from data_processor import ureg
 from .energy_consumption import EnergyConsumption
 
 # pylint: disable=redefined-outer-name
@@ -12,7 +14,7 @@ from .energy_consumption import EnergyConsumption
 
 @pytest.fixture
 def sample_energy_df():
-    return pd.DataFrame({
+    df = pd.DataFrame({
         "host": ["radxax4", "radxax4"],
         "tool": ["gzip", "gzip"],
         "dataset": ["image", "image"],
@@ -24,14 +26,20 @@ def sample_energy_df():
         "real": [10.0, 10.0],
         "power": [10.0, 12.0],
     })
+    df["energy"] = df["energy"].astype("pint[joule]")
+    df["real"] = df["real"].astype("pint[second]")
+    df["power"] = df["power"].astype("pint[watt]")
+    return df
 
 
 @pytest.fixture
 def sample_idle_power_df():
-    return pd.DataFrame({
+    df = pd.DataFrame({
         "host": ["radxax4"],
         "average_power": [5.0],
     })
+    df["average_power"] = df["average_power"].astype("pint[watt]")
+    return df
 
 
 @pytest.fixture
@@ -44,7 +52,7 @@ def test_energy_total(energy_consumption, sample_energy_df, sample_idle_power_df
 
     radxax4_row = result[result["host"] == "radxax4"].iloc[0]
     assert radxax4_row["num_runs"] == 2
-    assert radxax4_row["average_energy_total"] == 110.0
+    assert radxax4_row["average_energy_total"] == 110.0 * ureg.joule
 
 
 def test_energy_net(energy_consumption, sample_energy_df, sample_idle_power_df):
@@ -52,4 +60,4 @@ def test_energy_net(energy_consumption, sample_energy_df, sample_idle_power_df):
 
     radxax4_row = result[result["host"] == "radxax4"].iloc[0]
     assert radxax4_row["num_runs"] == 2
-    assert radxax4_row["average_energy_net"] == 60.0
+    assert radxax4_row["average_energy_net"] == 60.0 * ureg.joule
