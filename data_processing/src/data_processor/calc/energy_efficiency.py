c3b5b4a6aa0098001b69bcdb41b4b4f1392bbab1
Ralf Sieger <sieger@fzi.de>
2026-07-31 14:43:15 +0200

    added CSV export

--- data_processing/src/data_processor/calc/energy_efficiency.py
+++ data_processing/src/data_processor/calc/energy_efficiency.py
@@ -33,8 +33,8 @@ class EnergyEfficiency(Calculator):
         ).drop(columns=["_tool_key", "_strength_key"])
 
         self._print_table(energy_df)
-        #energy_file = "energy_efficiency_%s" % params.used_energy_file.stem.removeprefix("used_energy_") + ".csv"
-        #self._create_csv(energy_file, energy_df)
+        energy_file = "energy_efficiency_%s" % params.used_energy_file.stem.removeprefix("used_energy_") + ".csv"
+        self._create_csv(energy_file, energy_df)
 
     def _print_table(self, df: pd.DataFrame) -> None:
         table_df = df.copy()
