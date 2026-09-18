50b6fbd8d990252bfa961853b0b5872648e2af90
Ralf Sieger <sieger@fzi.de>
2026-09-18 10:02:15 +0200

    made energy calculation tests use the same input data

---
+++ data_aggregator/src/data_aggregator/calculate/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+import pandas as pd
+
+from data_aggregator.util.frame_io import FrameIO
+
+# pylint: disable=redefined-outer-name
+# pylint: disable=protected-access
+
+
+@pytest.fixture
+def run_data_single() -> pd.DataFrame:
+    data = """
+run,timestamp,voltage,current,power
+No Unit,No Unit,volt,ampere,watt
+1,2026-04-07 07:40:28.271,5.12425,0.49685,2.5459836125
+1,2026-04-07 07:40:28.281,5.12401,0.48916,2.5064607316
+1,2026-04-07 07:40:28.291,5.12401,0.4775,2.446714775
+"""
+    return _as_dataframe(data)
+
+
+@pytest.fixture
+def run_data_two(run_data_single) -> pd.DataFrame:
+    data = """
+run,timestamp,voltage,current,power
+No Unit,No Unit,volt,ampere,watt
+2,2026-04-07 07:41:05.513,5.12585,0.49288,2.526428948
+2,2026-04-07 07:41:05.523,5.12585,0.49189,2.5213543565
+2,2026-04-07 07:41:05.533,5.12585,0.51322,2.630688737
+"""
+    two = _as_dataframe(data)
+    return pd.concat([run_data_single, two])
+
+
+def _as_dataframe(data: str) -> pd.DataFrame:
+    io = FrameIO()
+    return io.load_str(data)
--- data_aggregator/src/data_aggregator/calculate/energy_calculator_test.py
+++ data_aggregator/src/data_aggregator/calculate/energy_calculator_test.py
@@ -10,38 +10,6 @@ from .energy_calculator import EnergyCalculator
 # pylint: disable=protected-access
 
 
-@pytest.fixture
-def run_data_single():
-    data = """
-run,timestamp,voltage,current,power
-No Unit,No Unit,volt,ampere,watt
-1,2026-04-07 07:40:28.271,5.12425,0.49685,2.5459836125
-1,2026-04-07 07:40:28.281,5.12401,0.48916,2.5064607316
-1,2026-04-07 07:40:28.291,5.12401,0.4775,2.446714775
-"""
-    return data
-
-
-@pytest.fixture
-def run_data_two(run_data_single):
-    data = run_data_single + """
-2,2026-04-07 07:41:05.513,5.12585,0.49288,2.526428948
-2,2026-04-07 07:41:05.523,5.12585,0.49189,2.5213543565
-2,2026-04-07 07:41:05.533,5.12585,0.51322,2.630688737
-"""
-    return data
-
-
-@pytest.fixture
-def single_run_data_frame(run_data_single):
-    return _as_dataframe(run_data_single)
-
-
-@pytest.fixture
-def run_data_frame_two(run_data_two):
-    return _as_dataframe(run_data_two)
-
-
 def _as_dataframe(data: str) -> pd.DataFrame:
     io = FrameIO()
     return io.load_str(data)
@@ -52,7 +20,7 @@ def calculator():
     return EnergyCalculator()
 
 
-def test_calculate_power_single(calculator, single_run_data_frame):
+def test_calculate_power_single(calculator, run_data_single):
     data = """
 run,timestamp,voltage,current,power,power_duration,energy_used
 No Unit,No Unit,volt,ampere,watt,second,joule
@@ -62,7 +30,7 @@ No Unit,No Unit,volt,ampere,watt,second,joule
 """
     df_expected = _as_dataframe(data)
 
-    df_actual = calculator.calculate_energy(single_run_data_frame)
+    df_actual = calculator.calculate_energy(run_data_single)
 
     for column in ["voltage", "current", "power", "power_duration", "energy_used"]:
         assert_allclose(
@@ -73,7 +41,7 @@ No Unit,No Unit,volt,ampere,watt,second,joule
         )
 
 
-def test_calculate_power_double(calculator, run_data_frame_two):
+def test_calculate_power_double(calculator, run_data_two):
     data = """
 run,timestamp,voltage,current,power,power_duration,energy_used
 No Unit,No Unit,volt,ampere,watt,second,joule
@@ -86,7 +54,7 @@ No Unit,No Unit,volt,ampere,watt,second,joule
 """
     df_expected = _as_dataframe(data)
 
-    df_actual = calculator.calculate_energy(run_data_frame_two)
+    df_actual = calculator.calculate_energy(run_data_two)
 
     for column in ["voltage", "current", "power", "power_duration", "energy_used"]:
         assert_allclose(
