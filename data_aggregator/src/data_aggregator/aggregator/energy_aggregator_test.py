9dab4cd8502eab975e4ca8db0564794e7c30896a
Ralf Sieger <sieger@fzi.de>
2026-09-23 12:09:34 +0200

    removed duplicate code

--- data_aggregator/src/data_aggregator/aggregator/energy_aggregator_test.py
+++ data_aggregator/src/data_aggregator/aggregator/energy_aggregator_test.py
@@ -1,10 +1,10 @@
 from pathlib import Path
-from io import StringIO
 
 import pytest
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
+from data_aggregator.util.frame_io import FrameIO
 from .energy_aggregator import EnergyAggregator
 
 # pylint: disable=redefined-outer-name
@@ -33,19 +33,9 @@ def single_run_data_frame(run_data_single):
     return _as_dataframe(run_data_single)
 
 
-def _as_dataframe(data: str, times=True) -> pd.DataFrame:
-    df = pd.read_csv(StringIO(data), header=[0, 1])
-
-    names = df.columns.get_level_values(0)
-    units = df.columns.get_level_values(1)
-
-    df.columns = names  # flatten
-    if times:
-        df["timestamp"] = pd.to_datetime(df["timestamp"])
-    for col, unit in zip(names, units):  # apply units
-        if unit != "No Unit":
-            df[col] = df[col].astype(f"pint[{unit}]")
-    return df
+def _as_dataframe(data: str) -> pd.DataFrame:
+    io = FrameIO()
+    return io.load_str(data)
 
 
 @pytest.fixture
@@ -58,7 +48,7 @@ def test_aggregate_energy_single(aggregator, single_run_data_frame):
 host,tool,dataset,mode,strength,threading,run,duration,energy,size,average_run_power
 No Unit,No Unit,No Unit,No Unit,No Unit,No Unit,No Unit,second,joules,byte,watt
 raspi5,bzip2,sensor,compress,default,single,1,0.07,0.21341973333599978,1.0,3.0488533333714254"""
-    df_expected = _as_dataframe(data, times=False)
+    df_expected = _as_dataframe(data)
 
     df_actual = aggregator.aggregate_energy(single_run_data_frame)
 
