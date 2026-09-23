a1f386b57323e011f348d148a19b792d8fe1d1e7
Ralf Sieger <sieger@fzi.de>
2026-09-23 15:01:07 +0200

    marked temporary column names with an underscore

--- data_aggregator/src/data_aggregator/calculate/trapezoid_energy_calculator.py
+++ data_aggregator/src/data_aggregator/calculate/trapezoid_energy_calculator.py
@@ -14,11 +14,11 @@ class TrapezoidEnergyCalculator(EnergyCalculator):
         df["power_duration"] = df.groupby("run")["timestamp"].diff().dt.total_seconds().astype("pint[second]")
 
         # average power between previous and current row
-        df["power_avg"] = (df["power"].shift() + df["power"]) / 2
+        df["_power_avg"] = (df["power"].shift() + df["power"]) / 2
         # trapezoid energy per row
-        df["energy_used"] = df["power_avg"] * df["power_duration"]
+        df["energy_used"] = df["_power_avg"] * df["power_duration"]
 
         df["energy_used"] = df["energy_used"].pint.to("joule")
-        del df['power_avg']
+        del df['_power_avg']
 
         return df
