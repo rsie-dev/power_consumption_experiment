8a4cc10af0e1555f585c6a4d0b65ad356ff07e43
Ralf Sieger <sieger@fzi.de>
2026-09-23 11:17:51 +0200

    EnergyCalculator is now an interface

--- data_aggregator/src/data_aggregator/calculate/energy_calculator.py
+++ data_aggregator/src/data_aggregator/calculate/energy_calculator.py
@@ -1,14 +1,10 @@
-import logging
+from abc import ABC, abstractmethod
 
 import pandas as pd
 
 
-class EnergyCalculator:
-    def __init__(self):
-        self._logger = logging.getLogger(self.__class__.__name__)
+class EnergyCalculator(ABC):
 
+    @abstractmethod
     def calculate_energy(self, df: pd.DataFrame) -> pd.DataFrame:
-        df["power_duration"] = df.groupby("run")["timestamp"].diff().dt.total_seconds().astype("pint[second]")
-        df["energy_used"] = df["power"] * df["power_duration"]
-        df["energy_used"] = df["energy_used"].pint.to("joule")
-        return df
+        pass
